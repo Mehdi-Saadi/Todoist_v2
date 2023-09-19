f43f189586000396e60a2108d43e7dac7e0c2d25
Mehdi <mehdi.0.saadi@gmail.com>
2023-09-20 00:33:41 +0330

    make out dated tasks red

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -69,7 +69,11 @@ if (! function_exists('controlDate')) {
             default:
                 $day = date('d M', $date);
                 $title = date('D d M Y', $date);
-                $color = '';
+                if ($date < $today) {
+                    $color = 'text-red-550';
+                } else {
+                    $color = '';
+                }
         }
 
         return '<span class="flex items-center ' . $color . '" id="task-deadline-' . $task->id . '" title="' . $title . '"><svg class="w-4 h-4 mr-0.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M18 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2zM5 6a1 1 0 011-1h12a1 1 0 011 1v12a1 1 0 01-1 1H6a1 1 0 01-1-1V6zm12 10a1 1 0 11-2 0 1 1 0 012 0zM7 8a.5.5 0 000 1h10a.5.5 0 000-1H7z" fill="currentColor"></path></svg>' . $day .'</span>';
