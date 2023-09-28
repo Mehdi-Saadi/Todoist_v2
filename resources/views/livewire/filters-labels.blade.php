6181ae07370d0ffa3f8284fbd22c5649af40f036
Mehdi <mehdi.0.saadi@gmail.com>
2023-09-28 15:24:33 +0330

    new label form added

--- resources/views/livewire/filters-labels.blade.php
+++ resources/views/livewire/filters-labels.blade.php
@@ -34,5 +34,46 @@
         </div>
         {{-- filters --}}
         <div></div>
+
+        {{-- new label form --}}
+        <div id="" class="fixed inset-0 flex items-center justify-center z-30">
+            {{-- overlay --}}
+            <div class="absolute inset-0 bg-stone-900 bg-opacity-50"></div>
+            {{-- modal --}}
+            <div class="max-w-md w-full bg-white sm:mx-auto mx-6 md:mt-24 mt-32 mb-auto rounded-xl shadow-xl z-30">
+                <div class="px-4 flex justify-between items-center py-2 border-b text-gray-800">
+                    <h5 class="text-xl font-bold">Add label</h5>
+
+                </div>
+                <form class="pb-4 flex flex-col text-gray-800 space-y-5">
+                    <input type="hidden" name="color" value="#808080" id="new-label-form-color">
+                    <div class="flex flex-col space-y-1 px-4">
+                        <label for="label-name" class="font-bold text-sm">Label name</label>
+                        <input type="text" name="name" class="rounded border-gray-300 focus:border-gray-400 focus:ring-0 w-full h-7 px-1" id="label-name" autocomplete="off">
+                    </div>
+                    <div class="px-4">
+                        <span class="font-bold text-sm">Label color</span>
+                        <x-dropdown.main>
+                            <button type="button" class="rounded border border-gray-300 focus:border-gray-400 focus:ring-0 w-full h-7 px-2 flex items-center text-sm" data-dropdown-toggle="new-label-form-all-labels"><x-icon.dot class="w-4 h-4 mr-2" style="color: #808080"/>Charcoal</button>
+                            <x-dropdown.menu class="w-full max-h-72 overflow-auto" id="new-label-form-all-labels">
+                                @foreach(\App\Models\Color::all() as $color)
+                                    <x-dropdown.button type="button" class="items-center text-sm"><x-icon.dot class="w-4 h-4 mr-2" style="color: {{ $color->code }}"/>{{ $color->name }}</x-dropdown.button>
+                                @endforeach
+                            </x-dropdown.menu>
+                        </x-dropdown.main>
+                    </div>
+                    {{-- add to favorites --}}
+                    <div></div>
+
+                    <div class="text-xs font-semibold pt-4 border-t mt-auto flex">
+                        <div class="px-4 ml-auto space-x-3">
+                            <button type="button" onclick="hideForm('add-task-btn', 'new-label-form')" class="bg-zinc-100 hover:bg-zinc-200 w-16 h-8 text-center rounded-md">Cancel</button>
+                            <button type="button" data-id="submit-btn" class="bg-red-550 enabled:hover:bg-red-850 w-16 h-8 text-center text-white rounded-md disabled:bg-opacity-50" disabled>Add</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
     </div>
 </div>
