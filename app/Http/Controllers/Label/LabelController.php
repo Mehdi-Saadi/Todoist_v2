f11857f25c8818872162461acef5aed8c33d174b
Mehdi <mehdi.0.saadi@gmail.com>
2023-09-29 12:12:02 +0330

    create method in label controller

--- app/Http/Controllers/Label/LabelController.php
+++ app/Http/Controllers/Label/LabelController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers\Label;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class LabelController extends Controller
 {
-    public function create(Request $request)
+    public function create(Request $request): JsonResponse
     {
         $user = auth()->user();
 
+        $data = $request->validate([
+            'color' => [Rule::exists('colors', 'code')],
+            'name' => ['required'],
+        ]);
 
+        $data['order'] = $user->labels()->max('order');
+        ++$data['order'];
 
+        $label = $user->labels()->create($data);
+
+        return response()->json($label);
     }
 
     public function delete(Request $request)
