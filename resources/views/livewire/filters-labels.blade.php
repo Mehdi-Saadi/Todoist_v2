b19638de357a3f5b2c91c2062cfca8cba2d344fa
Mehdi <mehdi.0.saadi@gmail.com>
2023-09-29 12:09:53 +0330

    show saved labels

--- resources/views/livewire/filters-labels.blade.php
+++ resources/views/livewire/filters-labels.blade.php
@@ -18,18 +18,20 @@
             </div>
             <hr class="my-1 ml-8">
             <div class="ml-8 @if($labelsIsClose === true) hidden @endif" id="labels-root" data-is-close="@if($labelsIsClose === true){{ 'true' }}@else{{ 'false' }}@endif">
-                <a href="#" class="flex justify-between items-center py-2 px-3 border rounded-lg mt-1 group" id="">
-                    <div class="flex items-center">
-                        <x-icon.tag class="w-6 h-6 mr-1"/>
-                        <span class="text-gray-800">Reading</span>
-                    </div>
-                    {{-- tools section --}}
-                    <div class="hidden md:flex invisible md:group-hover:visible">
-                        <x-button.header-tool title="Add to favorites" class="p-1"><x-icon.heart class="w-5 h-5"/></x-button.header-tool>
-                        <x-button.header-tool title="Edit label" class="p-1"><x-icon.pencil class="w-5 h-5"/></x-button.header-tool>
-                        <x-button.header-tool title="Delete label" class="p-1"><x-icon.trash class="w-5 h-5"/></x-button.header-tool>
-                    </div>
-                </a>
+                @foreach(auth()->user()->labels as $label)
+                    <a href="#" class="flex justify-between items-center py-2 px-3 border rounded-lg mt-1 group" id="{{ $label->id }}">
+                        <div class="flex items-center">
+                            <x-icon.tag class="w-6 h-6 mr-1" style="color: {{ $label->color }}"/>
+                            <span class="text-gray-800">{{ $label->name }}</span>
+                        </div>
+                        {{-- tools section --}}
+                        <div class="hidden md:flex invisible md:group-hover:visible">
+                            <x-button.header-tool title="Add to favorites" class="p-1"><x-icon.heart class="w-5 h-5"/></x-button.header-tool>
+                            <x-button.header-tool title="Edit label" class="p-1"><x-icon.pencil class="w-5 h-5"/></x-button.header-tool>
+                            <x-button.header-tool title="Delete label" class="p-1"><x-icon.trash class="w-5 h-5"/></x-button.header-tool>
+                        </div>
+                    </a>
+                @endforeach
             </div>
         </div>
         {{-- filters --}}
