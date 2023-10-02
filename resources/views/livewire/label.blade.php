69c68773b538a6c147ea65a349f07736c9c6f21a
Mehdi <mehdi.0.saadi@gmail.com>
2023-10-02 18:04:24 +0330

    use custom-todo instead of label-todo

--- resources/views/livewire/label.blade.php
+++ resources/views/livewire/label.blade.php
@@ -2,7 +2,7 @@
     {{-- header --}}
     <x-layouts.header class="border-b">
         <div class="mr-auto">
-            <h2 class="font-bold text-gray-800 text-xl">Inbox</h2>
+            <h2 class="font-bold text-gray-800 text-xl">{{ $label->name }}</h2>
         </div>
         <div class="ml-auto flex text-xs font-medium space-x-2">
             <x-button.header-tool class="py-1 px-2"><x-icon.adjustments-horizontal class="w-6 h-6 mr-1"/><span class="hidden lg:flex">View</span></x-button.header-tool>
@@ -22,7 +22,7 @@
     {{-- the 'grandParentTask' id is for stopping the loop of getting parents in 'doneCircleEffect.js' file --}}
     <div class="mx-2 mt-2 flex flex-col" id="grandParentTask">
         <x-task.sections.children id="nestedRoot">
-            <x-task.todo :$tasks/>
+            <x-task.custom-todo :$tasks/>
         </x-task.sections.children>
     </div>
     <div class="px-7 mt-5 pb-20">
