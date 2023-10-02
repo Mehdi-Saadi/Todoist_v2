23c080d016e3bccfb8aa548cc9b7b4961797db87
Mehdi <mehdi.0.saadi@gmail.com>
2023-10-02 18:06:42 +0330

    show name of label, logic of showing tasks of label changed

--- app/Livewire/Label.php
+++ app/Livewire/Label.php
@@ -8,11 +8,13 @@ use App\Models\Label as LabelModel;
 class Label extends Component
 {
     public $tasks;
+    public $label;
 
-    public function mount(LabelModel $label)
+    public function mount(LabelModel $label): void
     {
         $this->authorize('view', $label);
-        $this->tasks = $label->tasks()->where('parent_id', 0)->where('is_done', 0)->get();
+        $this->tasks = $label->tasks()->where('is_done', 0)->get();
+        $this->label = $label;
     }
 
     public function render()
