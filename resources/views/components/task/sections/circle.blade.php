385e5d071b40b908bc97131df3b2fe19f557b70f
Mehdi <mehdi.0.saadi@gmail.com>
2023-10-03 10:56:43 +0330

    circle icon updated

--- resources/views/components/task/sections/circle.blade.php
+++ resources/views/components/task/sections/circle.blade.php
@@ -1,1 +1,1 @@
-<x-icon.circle class="w-6 h-6 mr-1 rounded-full" style="color: {{ $task->color }}" id="task-circle-{{ $task->id }}"/>
+<x-icon.circle class="w-5 h-5 mr-1 rounded-full" style="color: {{ $task->color }}; background: {{ $task->color }}1f;" id="task-circle-{{ $task->id }}"/>
