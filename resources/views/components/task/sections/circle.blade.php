f53ae9609be8cf2665296d6e36acdc929aa328cd
Mehdi <mehdi.0.saadi@gmail.com>
2023-10-04 14:27:05 +0330

    circle svg updated

--- resources/views/components/task/sections/circle.blade.php
+++ resources/views/components/task/sections/circle.blade.php
@@ -1,4 +1,4 @@
 <div class="relative w-5 h-5 mr-2 group/circle">
-    <x-icon.circle class="w-full h-full absolute" style="color: {{ $task->color }};background: {{ $task->color }}17;" id="task-circle-{{ $task->id }}"/>
-    <x-icon.circle-check class="opacity-0 w-5 h-5 absolute group-hover/circle:opacity-100 rounded-full transition duration-300" style="color: {{ $task->color }};background: {{ $task->color }}1f;"/>
+    <x-icon.circle class="w-full h-full absolute" style="color: {{ $task->color }};background: {{ $task->color }}14;" id="task-circle-{{ $task->id }}"/>
+    <x-icon.circle-check class="opacity-0 w-5 h-5 absolute group-hover/circle:opacity-100 rounded-full transition duration-300" style="color: {{ $task->color }};"/>
 </div>
